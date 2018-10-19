501efb7c32cc3951287c54162f011972ca894d3b
Richi Tank <richitank4@gmail.com>
2018-10-19 15:33:26 -0400

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormsModule } from "@angular/forms";
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'my-app';
+  todoArray=[]
+
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+
+  todoSubmit(value:any){
+    if(value!==""){
+   this.todoArray.push(value.todo)
+    //this.todoForm.reset()
+   }else{
+     alert('Field required **')
+   }
+   
+ }
+
+  deleteItem(todo){
+    for(let i=0; i<=this.todoArray.length; i++){
+      if(todo == this.todoArray[i]){
+        this.todoArray.splice(i,1)
+      }
+    }
+  }
+  
 }
